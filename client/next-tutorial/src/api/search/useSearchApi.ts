bbd556fdca2602ba9b17d0f5ef61fbaccbf7f298
Jaylin <jaylin6115@gmail.com>
2024-06-20 21:50:28 +0900

    [FE] 검색 백엔드 GET API 연결 #55

---
+++ client/next-tutorial/src/api/search/useSearchApi.ts
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../axiosConfig";
+
+const getSearchResult = async (keyword: string) => {
+  const result = await api({
+    method: "get",
+    url: `/search?keyword=${keyword}`,
+  });
+
+  return result.data.result;
+};
+
+export const useGetSearch = (keyword: string) => {
+  const queryKey = ["useGetSearch", keyword];
+
+  return useQuery({
+    queryKey,
+    queryFn: () => getSearchResult(keyword),
+  });
+};
