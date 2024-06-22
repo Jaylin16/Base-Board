01fbf4e1b98bc6fe0c024c6ad3e0aaac8ac72434
Jaylin <jaylin6115@gmail.com>
2024-06-23 01:56:33 +0900

    [FE] 각 페이지 동적 import 방식으로 변경 #22

---
+++ client/next-tutorial/src/component/pages/board/DetailPage.tsx
@@ -0,0 +1,349 @@
+"use client";
+
+import { useGetBoardDetail } from "@/api/board/useBoardApi";
+import {
+  useCreateComment,
+  useDeleteComment,
+  useUpdateComment,
+} from "@/api/comment/useCommentApi";
+import { css } from "@emotion/react";
+import { ReadonlyURLSearchParams } from "next/navigation";
+import { useState } from "react";
+
+interface commentType {
+  _id: string;
+  commentWriterId: string;
+  commentWriterNickname: string;
+  commentContent: string;
+  createdAt: Date;
+}
+
+interface DetailPageProps {
+  searchParams: ReadonlyURLSearchParams;
+}
+
+const DetailPage = ({ searchParams }: DetailPageProps) => {
+  const boardId = searchParams.get("item_id") as string;
+
+  const [commentContent, setCommentContent] = useState<string>();
+  const [editCommentId, setEditCommentId] = useState<string | null>(null);
+  const [editComment, setEditComment] = useState<string>();
+
+  const { data, isLoading, refetch } = useGetBoardDetail(boardId);
+
+  const createComment = useCreateComment();
+  const updateComment = useUpdateComment();
+  const deleteComment = useDeleteComment();
+
+  const content = data?.targetBoard;
+
+  const boardIntlOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  };
+
+  const commentIntlOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formatDate = (date: Date, IntlOption: Intl.DateTimeFormatOptions) => {
+    const newDate = new Date(date);
+    return new Intl.DateTimeFormat("ko", IntlOption).format(newDate);
+  };
+
+  /* 여기부터 이벤트 핸들러 모음 입니다. */
+
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setCommentContent(value);
+  };
+
+  const commentEditHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setEditComment(value);
+  };
+
+  /* 여기부터 버튼 핸들러 모음 입니다. */
+
+  const submitButtonHandler = () => {
+    const data = {
+      commentContent: commentContent,
+      boardId: boardId,
+    };
+
+    createComment.mutate(data, {
+      onSuccess: () => {
+        setCommentContent("");
+      },
+      onError: () => {
+        refetch();
+      },
+    });
+  };
+
+  const editButtonHandler = (commentId: string, commentContent: string) => {
+    if (editCommentId === null) {
+      setEditCommentId(commentId);
+      setEditComment(commentContent);
+    } else {
+      setEditCommentId(null);
+    }
+  };
+
+  const saveEditCommentButtonHandler = (commentId: string) => {
+    const data = {
+      commentId: commentId,
+      commentContent: editComment,
+    };
+
+    updateComment.mutate(data, {
+      onSuccess: () => {
+        setEditCommentId(null);
+      },
+    });
+  };
+
+  const deleteButtonHandler = (commentId: string) => {
+    deleteComment.mutate(commentId);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <div css={rootStyle}>
+        <div css={boardTitleStyle}>
+          📝 {content?.boardType.toUpperCase()} 게시판
+          <div css={fristLineStyle}>
+            <div css={titleWrapper}>
+              <span>{content?.boardCategory}</span>
+              <span>{content?.boardTitle}</span>
+            </div>
+
+            <div css={hitWrapper}>
+              <span>👀 {content?.hit}</span>
+              <span>💬 {content?.comments.length}</span>
+            </div>
+          </div>
+          <div css={secondLineStyle}>
+            <div>작성자: {content?.boardWriterNickname}</div>
+            <div>
+              작성 일자: {formatDate(content?.createdAt, boardIntlOptions)}
+            </div>
+          </div>
+        </div>
+
+        <div css={contentBoxStyle}>
+          <div
+            dangerouslySetInnerHTML={{ __html: content.boardContents }}
+          ></div>
+        </div>
+
+        <div>
+          <div css={totalCommentStyle}>💬 {content?.comments.length}</div>
+
+          {content?.comments?.map((comment: commentType) => {
+            return (
+              <div key={comment._id}>
+                <div css={commentStyle}>
+                  <span> {comment.commentWriterNickname} </span>
+                  <span>
+                    {formatDate(comment.createdAt, commentIntlOptions)}
+                  </span>
+
+                  <button
+                    css={commentEditButtonStyle}
+                    onClick={() => {
+                      editButtonHandler(comment._id, comment.commentContent);
+                    }}
+                  >
+                    {comment._id === editCommentId ? "취소" : "수정"}
+                  </button>
+
+                  <button
+                    css={saveButtonStyle(editCommentId, comment._id)}
+                    onClick={() => {
+                      saveEditCommentButtonHandler(comment._id);
+                    }}
+                  >
+                    {"저장"}
+                  </button>
+
+                  <button
+                    css={commentEditButtonStyle}
+                    onClick={() => {
+                      deleteButtonHandler(comment._id);
+                    }}
+                  >
+                    삭제
+                  </button>
+
+                  <div css={commentContentStyle(editCommentId, comment._id)}>
+                    {comment._id !== editCommentId ? (
+                      <div> {comment.commentContent} </div>
+                    ) : (
+                      <input
+                        name="editTextBox"
+                        type="text"
+                        value={editComment}
+                        onChange={commentEditHandler}
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+
+          <div css={commentInputWrapper}>
+            <input
+              type="text"
+              placeholder="댓글을 입력해주세요."
+              name="commentContent"
+              value={commentContent}
+              onChange={inputHandler}
+            />
+            <button css={commentButton} onClick={submitButtonHandler}>
+              댓글 등록
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailPage;
+
+const rootStyle = css`
+  height: calc(100vh - 97px);
+  padding: 27px 10%;
+`;
+
+const boardTitleStyle = css`
+  font-size: 18px;
+`;
+
+const fristLineStyle = css`
+  color: #555555;
+  border-top: 1.5px solid #1d3d65;
+  border-bottom: 1px solid #d9d9d9;
+  padding: 16px 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const hitWrapper = css`
+  display: flex;
+  gap: 42.5px;
+`;
+
+const titleWrapper = css`
+  display: flex;
+  gap: 86px;
+`;
+
+const secondLineStyle = css`
+  color: #555555;
+  border-bottom: 1px solid #d9d9d9;
+  padding: 8px 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const contentBoxStyle = css`
+  min-height: 500px;
+  padding: 50px 30px;
+`;
+
+const totalCommentStyle = css`
+  border-bottom: 1px solid #999999;
+  padding-bottom: 17px;
+`;
+
+const commentStyle = css`
+  border-bottom: 1px solid #d9d9d9;
+  padding: 13px 17px 34px 17px;
+
+  span {
+    margin-right: 11px;
+    color: #999999;
+    font-size: 13px;
+  }
+
+  span:first-child {
+    font-size: 18px;
+  }
+`;
+
+const commentEditButtonStyle = css`
+  margin: 0 5px;
+
+  color: #999999;
+  cursor: pointer;
+`;
+
+const saveButtonStyle = (
+  commentEditMode: string | null,
+  commentId: string
+) => css`
+  display: ${commentEditMode !== commentId && "none"};
+
+  color: #999999;
+  cursor: pointer;
+`;
+
+const commentButton = css`
+  background: #7d9dcf;
+  font-size: 18px;
+  height: 50px;
+  width: 155.71px;
+  border-radius: 5px;
+  padding: 7.14px 30px;
+
+  cursor: pointer;
+`;
+
+const commentInputWrapper = css`
+  display: flex;
+  gap: 14px;
+  padding: 42px 0 56px 0;
+
+  input {
+    border-radius: 5px;
+    border: 1px solid #999999;
+    box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.25);
+    padding-left: 24px;
+
+    height: 51px;
+    width: 100%;
+
+    ::placeholder {
+      font-size: 18px;
+      color: #999999;
+    }
+  }
+`;
+
+const commentContentStyle = (editCommentId: string | null, commentId: string) =>
+  css`
+    padding-top: 23px;
+    font-size: 18px;
+
+    input {
+      font-size: 18px;
+      width: 100%;
+      height: 100%;
+    }
+  `;
---
+++ client/next-tutorial/src/component/pages/board/NoticePage.tsx
@@ -0,0 +1,240 @@
+"use client";
+import { useGetBoardList } from "@/api/board/useBoardApi";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { css } from "@emotion/react";
+import { useEffect, useState } from "react";
+import { Pagination, PaginationItem } from "@mui/material";
+
+const NoticePage = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+  const { data, refetch } = useGetBoardList("공지", page, pageSize);
+
+  const formatDate = (date: Date) => {
+    const newDate = new Date(date);
+
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+
+    return new Intl.DateTimeFormat("ko", options).format(newDate);
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [page]);
+
+  const onPageChange = (event: React.ChangeEvent<any>, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const onClickWriteButton = () => {
+    const currentParmas = Object.fromEntries(searchParams);
+    let newSearchParmas = { ...currentParmas };
+    newSearchParmas = {
+      ...currentParmas,
+      main: `${pathname.slice(1)}`,
+      page: `write`,
+    };
+
+    router.push(`?${new URLSearchParams(newSearchParmas)}`);
+  };
+
+  const onClickDetailButton = (id: string) => {
+    const currentParmas = Object.fromEntries(searchParams);
+    let newSearchParmas = { ...currentParmas };
+    newSearchParmas = {
+      ...currentParmas,
+      main: `${pathname.slice(1)}`,
+      page: `detail`,
+      item_id: id,
+    };
+
+    router.push(`?${new URLSearchParams(newSearchParmas)}`);
+  };
+
+  return (
+    <>
+      <div css={rootStyle}>
+        <div css={titleWrapper}>
+          <div css={titleStyle}> 📌 공지사항 </div>
+          <div css={writeButton} onClick={onClickWriteButton}>
+            ✏️ 글 작성
+          </div>
+        </div>
+
+        <div>
+          <div css={firstLineStyle}>
+            <span className="noStyle">no</span>
+            <span className="categoryStyle">카테고리</span>
+            <span className="titleStyle">제목</span>
+            <span className="dateStyle">날짜</span>
+            <span className="hitStyle">조회</span>
+          </div>
+
+          <div>
+            {data &&
+              data?.boardList?.map(
+                (content: any, index: number, value: string) => {
+                  return (
+                    <div
+                      css={lineStyle(index + 1 === value.length)}
+                      key={content._id}
+                      onClick={() => {
+                        onClickDetailButton(content._id);
+                      }}
+                    >
+                      <span className="noStyle"> {content.boardId} </span>
+                      <span className="categoryStyle">
+                        {content.boardCategory}
+                      </span>
+                      <span css={listTitleStyle}> {content.boardTitle} </span>
+                      <span className="dateStyle">
+                        {formatDate(content.createdAt)}
+                      </span>
+                      <span className="hitStyle"> {content.hit} </span>
+                    </div>
+                  );
+                }
+              )}
+          </div>
+        </div>
+
+        <div css={pageWrapper}>
+          <Pagination
+            count={data?.totalPages}
+            page={page}
+            onChange={onPageChange}
+            size="medium"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "15px 0",
+            }}
+            renderItem={(item) => (
+              <PaginationItem {...item} sx={{ fontSize: 12 }} />
+            )}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NoticePage;
+
+const rootStyle = css`
+  height: calc(100vh - 97px);
+  padding: 35px 10%;
+`;
+
+const titleStyle = css`
+  color: #1d3d65;
+`;
+
+const writeButton = css`
+  width: 92px;
+  height: 35px;
+  background: #7d9dcf;
+  border-radius: 5px;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 9px;
+  cursor: pointer;
+`;
+
+const titleWrapper = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const firstLineStyle = css`
+  background: rgba(219, 176, 52, 0.5);
+  border-top: 1.5px solid #1d3d65;
+  padding: 16px 0px;
+  display: flex;
+  justify-content: space-around;
+
+  .noStyle {
+    width: 7%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .categoryStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .titleStyle {
+    width: 63%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .dateStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .hitStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const lineStyle = (isLast: boolean) => css`
+  border-bottom: ${!isLast && "1px solid #d9d9d9"};
+  height: 58px;
+  display: flex;
+  align-items: center;
+
+  .noStyle {
+    width: 7%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .categoryStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .dateStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .hitStyle {
+    width: 10%;
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const listTitleStyle = css`
+  width: 63%;
+  justify-content: flex-start;
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+const pageWrapper = css`
+  bottom: 40px;
+  left: 50%;
+`;
